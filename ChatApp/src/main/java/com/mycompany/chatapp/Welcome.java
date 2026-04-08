6ec72d342933ec2741298597128c11c25059be95
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-04-08 22:08:41 +0200

    Fixed folder structure

--- ChatApp/src/main/java/com/mycompany/chatapp/Welcome.java
+++ ChatApp/src/main/java/com/mycompany/chatapp/Welcome.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.chatapp;
-
-/**
- *
- * @author User
- */
-public class Welcome {
+public class Login {
+    private String username;
+    private String password;
+    private String cellNo;
+    private String name;
+    private String surname;
     
+    public boolean checkUserName(String username) {
+        return false;
+    }
+    
+    public boolean checkPassword(String password) {
+        return false;
+    }
+    
+    public boolean checkCellNo(String cellNo) {
+        return false;
+    }
+    
+    public String userReg(String name, String surname, String username, String password, String cellNo) {
+        return "";
+    }
+    
+    public boolean login(String username, String password) {
+        return false;
+    }
+    
+    public String returnLogin(String username, String password) {
+        return "";
+    }
 }
