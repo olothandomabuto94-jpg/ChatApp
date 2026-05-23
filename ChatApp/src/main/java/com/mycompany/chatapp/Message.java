29a0e7932a0cb0c72b033c5d65456a1f1525e963
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-05-23 20:08:16 +0200

    Fixed messaeg.java

--- ChatApp/src/main/java/com/mycompany/chatapp/Message.java
+++ ChatApp/src/main/java/com/mycompany/chatapp/Message.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.chatapp;
 
-/**
- *
- * @author User
- */
 public class Message {
-    
+
+    private String recipient;
+    private String messageText;
+    private String messageID;
+    private String messageHash;
+
+    private static int messageCount = 0;
+
+    public Message(String recipient, String messageText) {
+        this.recipient = recipient;
+        this.messageText = messageText;
+        this.messageID = generateID();
+        this.messageHash = createHash();
+        messageCount++;
+    }
+
+    private String generateID() {
+        return String.valueOf((int)(Math.random() * 9000) + 1000);
+    }
+
+    private String createHash() {
+        String[] words = messageText.trim().split("\\s+");
+
+        String first = words.length > 0 ? words[0] : "";
+        String last = words.length > 0 ? words[words.length - 1] : "";
+
+        return (messageID + ":" + messageCount + ":" + first + last).toUpperCase();
+    }
+
+    public String getMessageDetails() {
+        return "ID: " + messageID +
+               "\nHash: " + messageHash +
+               "\nTo: " + recipient +
+               "\nMessage: " + messageText;
+    }
+
+    public static int getTotalMessages() {
+        return messageCount;
+    }
 }
