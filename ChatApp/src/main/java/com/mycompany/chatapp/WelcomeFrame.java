410603a123ee7ded5a0ffb832d7c745c277d7761
Olothando Mabuto <olothandomabuto94@gmail.com>
2026-05-23 20:15:35 +0200

    Created message frame file and updated welcome frame for part 2

--- ChatApp/src/main/java/com/mycompany/chatapp/WelcomeFrame.java
+++ ChatApp/src/main/java/com/mycompany/chatapp/WelcomeFrame.java
@@ -7,14 +7,23 @@ public class WelcomeFrame {
     public WelcomeFrame(String message) {
 
         JFrame window = new JFrame("Welcome");
-        window.setSize(400, 200);
+        window.setSize(400, 250);
         window.setLayout(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
 
         JLabel label = new JLabel("<html><center>" + message + "</center></html>");
-        label.setBounds(50, 50, 300, 50);
+        label.setBounds(50, 40, 300, 50);
         window.add(label);
+
+        JButton continueBtn = new JButton("Go to Chat");
+        continueBtn.setBounds(130, 130, 120, 30);
+        window.add(continueBtn);
+
+        continueBtn.addActionListener(e -> {
+            window.dispose();
+            new MessageFrame();
+        });
 
         window.setVisible(true);
     }
